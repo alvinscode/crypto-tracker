c123c350d5b9fa4b5c2cbd81955508baf1893d24
Alvin Li <aflip24@gmail.com>
2023-05-06 01:07:20 -0500

    add price to assets

--- index.js
+++ index.js
@@ -9,25 +9,16 @@ fetch('https://api.coincap.io/v2/assets')
     // Grab the top 3 assets
     const top3Assets = assets.slice(0, 3);
 
-    // Create an array of strings containing the id and changePercent24Hr values
-    const assetStrings = top3Assets.map(asset => `ID: ${asset.id}, Change: ${asset.changePercent24Hr}%`);
+    // Create an array of strings containing the ID, symbol, changePercent24Hr, and priceUSD values for the top assets
+    const topAssetStrings = top3Assets.map(asset => `ID: ${asset.id}, Symbol: ${asset.symbol}, Change: ${asset.changePercent24Hr}%, Price: $${asset.priceUsd}`);
 
-    // Append the strings to the 'crypto1' div
-    const crypto1Div = document.getElementById('hotcrypto');
-    assetStrings.forEach(assetString => {
+    // Append the strings to the 'hotcrypto' div
+    const hotcryptoDiv = document.getElementById('hotcrypto');
+    topAssetStrings.forEach(assetString => {
       const pElement = document.createElement('p');
       pElement.textContent = assetString;
-      crypto1Div.appendChild(pElement);
+      hotcryptoDiv.appendChild(pElement);
     });
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
-
-  fetch('https://api.coincap.io/v2/assets')
-  .then(response => response.json())
-  .then(data => {
-    const assets = data.data;
 
     // Sort assets by changePercent24Hr in ascending order
     assets.sort((a, b) => a.changePercent24Hr - b.changePercent24Hr);
@@ -35,12 +26,12 @@ fetch('https://api.coincap.io/v2/assets')
     // Grab the bottom 3 assets
     const bottom3Assets = assets.slice(0, 3);
 
-    // Create an array of strings containing the id and changePercent24Hr values
-    const assetStrings = bottom3Assets.map(asset => `ID: ${asset.id}, Change: ${asset.changePercent24Hr}%`);
+    // Create an array of strings containing the ID, symbol, changePercent24Hr, and priceUSD values for the bottom assets
+    const bottomAssetStrings = bottom3Assets.map(asset => `ID: ${asset.id}, Symbol: ${asset.symbol}, Change: ${asset.changePercent24Hr}%, Price: $${asset.priceUsd}`);
 
     // Append the strings to the 'coldcrypto' div
     const coldcryptoDiv = document.getElementById('coldcrypto');
-    assetStrings.forEach(assetString => {
+    bottomAssetStrings.forEach(assetString => {
       const pElement = document.createElement('p');
       pElement.textContent = assetString;
       coldcryptoDiv.appendChild(pElement);
