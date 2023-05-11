2b70892c57b6dc5817ccb7db2740f3e612560ad0
Alvin Li <aflip24@gmail.com>
2023-05-11 02:59:09 -0500

    add changes to make the assets load when page first opens, while still maintaining self update function

--- index.js
+++ index.js
@@ -5,49 +5,55 @@ function updateAssets() {
     container.appendChild(pElement);
   }
 
-  fetch('https://api.coincap.io/v2/assets', {
-    headers: {
-      'Accept-Encoding': 'gzip'
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      const assets = data.data;
-
-      // Sort assets by changePercent24Hr in descending order
-      assets.sort((a, b) => b.changePercent24Hr - a.changePercent24Hr);
-
-      // Grab the top 3 assets
-      const top3Assets = assets.slice(0, 3);
-
-      // Clear the 'hotcrypto' div before updating
-      const hotcryptoDiv = document.getElementById('hotcrypto');
-      hotcryptoDiv.innerHTML = '';
-
-      // Append the assets with line breaks to the 'hotcrypto' div
-      top3Assets.forEach(asset => {
-        appendAssetWithLineBreaks(asset, hotcryptoDiv);
-      });
-
-      // Sort assets by changePercent24Hr in ascending order
-      assets.sort((a, b) => a.changePercent24Hr - b.changePercent24Hr);
-
-      // Grab the bottom 3 assets
-      const bottom3Assets = assets.slice(0, 3);
-
-      // Clear the 'coldcrypto' div before updating
-      const coldcryptoDiv = document.getElementById('coldcrypto');
-      coldcryptoDiv.innerHTML = '';
-
-      // Append the assets with line breaks to the 'coldcrypto' div
-      bottom3Assets.forEach(asset => {
-        appendAssetWithLineBreaks(asset, coldcryptoDiv);
-      });
+  function fetchAndDisplayAssets() {
+    fetch('https://api.coincap.io/v2/assets', {
+      headers: {
+        'Accept-Encoding': 'gzip'
+      }
     })
-    .catch(error => {
-      console.log('Error:', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        const assets = data.data;
+
+        // Sort assets by changePercent24Hr in descending order
+        assets.sort((a, b) => b.changePercent24Hr - a.changePercent24Hr);
+
+        // Grab the top 3 assets
+        const top3Assets = assets.slice(0, 3);
+
+        // Clear the 'hotcrypto' div before updating
+        const hotcryptoDiv = document.getElementById('hotcrypto');
+        hotcryptoDiv.innerHTML = '';
+
+        // Append the assets with line breaks to the 'hotcrypto' div
+        top3Assets.forEach(asset => {
+          appendAssetWithLineBreaks(asset, hotcryptoDiv);
+        });
+
+        // Sort assets by changePercent24Hr in ascending order
+        assets.sort((a, b) => a.changePercent24Hr - b.changePercent24Hr);
+
+        // Grab the bottom 3 assets
+        const bottom3Assets = assets.slice(0, 3);
+
+        // Clear the 'coldcrypto' div before updating
+        const coldcryptoDiv = document.getElementById('coldcrypto');
+        coldcryptoDiv.innerHTML = '';
+
+        // Append the assets with line breaks to the 'coldcrypto' div
+        bottom3Assets.forEach(asset => {
+          appendAssetWithLineBreaks(asset, coldcryptoDiv);
+        });
+      })
+      .catch(error => {
+        console.log('Error:', error);
+      });
+  }
+
+  fetchAndDisplayAssets(); // Fetch and display assets initially
+
+  // Update assets every minute (60,000 milliseconds)
+  setInterval(fetchAndDisplayAssets, 60000);
 }
 
-// Update assets every minute (60,000 milliseconds)
-setInterval(updateAssets, 60000);
+updateAssets(); // Call the function to load assets when the page first opens
