40ce9b309ddec5aec4da5f23d1527698989efa5b
Alvin Li <aflip24@gmail.com>
2023-05-11 02:57:39 -0500

    add function to update the assets every minute on their own

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
-function appendAssetWithLineBreaks(asset, container) {
-  const pElement = document.createElement('p');
-  pElement.innerHTML = `ID: ${asset.id}<br>Symbol: ${asset.symbol}<br>Change: ${asset.changePercent24Hr}%<br>Price: $${asset.priceUsd}<br><br>`;
-  container.appendChild(pElement);
+function updateAssets() {
+  function appendAssetWithLineBreaks(asset, container) {
+    const pElement = document.createElement('p');
+    pElement.innerHTML = `ID: ${asset.id}<br>Symbol: ${asset.symbol}<br>Change: ${asset.changePercent24Hr}%<br>Price: $${asset.priceUsd}<br><br>`;
+    container.appendChild(pElement);
+  }
+
+  fetch('https://api.coincap.io/v2/assets', {
+    headers: {
+      'Accept-Encoding': 'gzip'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      const assets = data.data;
+
+      // Sort assets by changePercent24Hr in descending order
+      assets.sort((a, b) => b.changePercent24Hr - a.changePercent24Hr);
+
+      // Grab the top 3 assets
+      const top3Assets = assets.slice(0, 3);
+
+      // Clear the 'hotcrypto' div before updating
+      const hotcryptoDiv = document.getElementById('hotcrypto');
+      hotcryptoDiv.innerHTML = '';
+
+      // Append the assets with line breaks to the 'hotcrypto' div
+      top3Assets.forEach(asset => {
+        appendAssetWithLineBreaks(asset, hotcryptoDiv);
+      });
+
+      // Sort assets by changePercent24Hr in ascending order
+      assets.sort((a, b) => a.changePercent24Hr - b.changePercent24Hr);
+
+      // Grab the bottom 3 assets
+      const bottom3Assets = assets.slice(0, 3);
+
+      // Clear the 'coldcrypto' div before updating
+      const coldcryptoDiv = document.getElementById('coldcrypto');
+      coldcryptoDiv.innerHTML = '';
+
+      // Append the assets with line breaks to the 'coldcrypto' div
+      bottom3Assets.forEach(asset => {
+        appendAssetWithLineBreaks(asset, coldcryptoDiv);
+      });
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
 }
 
-fetch('https://api.coincap.io/v2/assets', {
-  headers: {
-    'Accept-Encoding': 'gzip'
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    const assets = data.data;
-
-    // Sort assets by changePercent24Hr in descending order
-    assets.sort((a, b) => b.changePercent24Hr - a.changePercent24Hr);
-
-    // Grab the top 3 assets
-    const top3Assets = assets.slice(0, 3);
-
-    // Append the assets with line breaks to the 'hotcrypto' div
-    const hotcryptoDiv = document.getElementById('hotcrypto');
-    top3Assets.forEach(asset => {
-      appendAssetWithLineBreaks(asset, hotcryptoDiv);
-    });
-
-    // Sort assets by changePercent24Hr in ascending order
-    assets.sort((a, b) => a.changePercent24Hr - b.changePercent24Hr);
-
-    // Grab the bottom 3 assets
-    const bottom3Assets = assets.slice(0, 3);
-
-    // Append the assets with line breaks to the 'coldcrypto' div
-    const coldcryptoDiv = document.getElementById('coldcrypto');
-    bottom3Assets.forEach(asset => {
-      appendAssetWithLineBreaks(asset, coldcryptoDiv);
-    });
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+// Update assets every minute (60,000 milliseconds)
+setInterval(updateAssets, 60000);
